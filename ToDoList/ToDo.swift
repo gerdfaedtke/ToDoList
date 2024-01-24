6be276d786c021644cb9f80d5b3e538aa691d682
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-24 19:23:57 +0100

    Ch. 5.6 A First Look at MVVM in SwiftUI (part of the ToDo List app tutorial)

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Gerd Faedtke on 24.01.24.
+//
+
+import Foundation
---
+++ ToDoList/ToDosViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDosViewModel.swift
+//  ToDoList
+//
+//  Created by Gerd Faedtke on 24.01.24.
+//
+
+import Foundation
