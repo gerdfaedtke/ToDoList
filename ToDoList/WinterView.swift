9e47bca96694e51324d0cdfc58d5ae08dea72cfa
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-24 11:28:26 +0100

    Ch. 5.2 SwiftUI Lists, List Styles, and Navigation Titles (To Do List app series)

---
+++ ToDoList/WinterView.swift
@@ -0,0 +1,18 @@
+//
+//  WinterView.swift
+//  ToDoList
+//
+//  Created by Gerd Faedtke on 24.01.24.
+//
+
+import SwiftUI
+
+struct WinterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WinterView()
+}
