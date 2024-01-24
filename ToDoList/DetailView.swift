477e1a31e12a0e466c9f84571feafb9026242c9f
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-24 10:29:20 +0100

    Ch. 5.1 A Beginner's Intro to the NavigationStack & NavigationLink in SwiftUI (To Do List app)

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Gerd Faedtke on 24.01.24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
