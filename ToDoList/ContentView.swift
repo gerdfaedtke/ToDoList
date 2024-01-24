86636344789fcc6b91e70a6982453216c6d920d3
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-24 09:59:29 +0100

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Gerd Faedtke on 24.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
